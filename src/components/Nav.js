22305814f2889fd3e4d05268d6e4d67c19dc0d12
longjichen1 <chenjerry222@gmail.com>
2022-01-11 16:27:51 -0600

    added animation to nav menu and blur effect

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,7 @@ import earthLogo from "../images/earth-logo.png";
 function Nav() {
   const [photoData, setPhotoData] = useState(null);
   const [blur, setBlur] = useState(false);
+  const [blurIcon, setBlurIcon] = useState(false);
     const generateRandomDate = () =>{
       const year = Math.floor(Math.random() * 18) + 1;
       const date = Math.floor(Math.random() * 27) + 1;
@@ -38,20 +39,29 @@ function Nav() {
     // }else{
     //   document.body.style.background = 'black';
     // }
-
+    function handleBlurIcon(){
+      if (blurIcon === true){
+        setBlurIcon(false);
+      }else{
+        setBlurIcon(true);
+      }
+      console.log(blurIcon);
+    }
   return(
     <>
-    <div className= {`h-screen ${blur? 'blur-sm saturate-50': ''} -z-10`} style={{backgroundImage:`url('https://coolwallpapers.me/picsup/5814157-meteor-wallpapers.jpg')`, backgroundPosition:'center',backgroundSize:'cover', position:'absolute', left:'0', right:'0', top:'0', bottom:'0'}}></div>
+    <div className= {`h-screen ${blur? 'blur-sm saturate-50': 'blur-none saturate-100'} -z-10 transition-all duration-300`} style={{backgroundImage:`url('https://coolwallpapers.me/picsup/5814157-meteor-wallpapers.jpg')`, backgroundPosition:'center',backgroundSize:'cover', position:'absolute', left:'0', right:'0', top:'0', bottom:'0'}}>
+      
+    </div>
     <div>
       <nav className = "flex items-center justify-between flex-wrap  p-4">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <img onClick = {() =>{blur? setBlur(false):setBlur(true)}}className = "p-0"src={earthLogo} style={{maxWidth: 70, margin:0}}></img>
-            <span className="font-semibold text-xl tracking-widest pl-2">GeoSphere</span>
+        <div className="p-4 flex items-center flex-shrink-0 text-white mr-6 ">
+            <img  onClick = {() =>{blur? setBlur(false):setBlur(true)}}className = {`${!blur? 'animate-pulse hover:outline-white': 'hover:outline-white'} delay-200 p-0  outline outline-4 outline-slate-900 outline-offset-4 rounded-full  ${blurIcon? 'animate-spin':''}`} src={earthLogo} style={{maxWidth: 70, margin:0}}></img>
+            <span className="font-semibold text-2xl tracking-widest pl-4">GeoSphere</span>
         </div>
         <div className = "block lg:hidden">
             
         </div>
-        
+        <p className="hover:visible translate-y-5 duration-1000 justify-center text-center">hello</p>
       </nav>
       
       {/* <button onClick = {() =>{blur? setBlur(false):setBlur(true)}} className = "bg-white text-black uppercase">CLICK</button> */}
